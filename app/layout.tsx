e61e3255cb6a34ea50d4d5887c2767f1bc368792
maetin <maetin0324@gmail.com>
2023-06-01 17:27:20 +0900

    move main tag to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import './globals.css';
+import './_components/Header';
+import './_components/Footer';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        {children}
+      </main>  
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,31 +6,29 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col items-center justify-center">
-        <img src="/maetin.jpg" alt="maetin" className="w-28 h-28 rounded-full" />
-        <h3 className="text-4xl font-bold mt-8">まえちん</h3>
-        <ul className="flex mt-4">
-          <li className="mr-4">
-            <Link
-              href="https://twitter.com/maetin_coins"
-              target="_blank"
-              className="text-blue-500 hover:underline"
-            >
-              <h5 className="text-5xl p-10">
-                <BsTwitter />
-              </h5>
-            </Link>
-          </li>
-          <li>
-            <Link href="https://github.com/maetin0324" target="_blank" className="hover:underline">
-              <h5 className="text-5xl p-10">
-                <AiFillGithub />
-              </h5>
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </main>
+    <div className="flex flex-col items-center justify-center">
+      <img src="/maetin.jpg" alt="maetin" className="w-28 h-28 rounded-full" />
+      <h3 className="text-4xl font-bold mt-8">まえちん</h3>
+      <ul className="flex mt-4">
+        <li className="mr-4">
+          <Link
+            href="https://twitter.com/maetin_coins"
+            target="_blank"
+            className="text-blue-500 hover:underline"
+          >
+            <h5 className="text-5xl p-10">
+              <BsTwitter />
+            </h5>
+          </Link>
+        </li>
+        <li>
+          <Link href="https://github.com/maetin0324" target="_blank" className="hover:underline">
+            <h5 className="text-5xl p-10">
+              <AiFillGithub />
+            </h5>
+          </Link>
+        </li>
+      </ul>
+    </div>
   );
 }
