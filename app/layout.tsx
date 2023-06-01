10ba8acd00c9f3f58152e0ffccdcf134984240a1
maetin <maetin0324@gmail.com>
2023-06-01 17:29:39 +0900

    apply format

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,9 +14,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ja">
       <body className={inter.className}>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        {children}
-      </main>  
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          {children}
+        </main>
       </body>
     </html>
   );
