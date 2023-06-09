81745bc9a61acdc176222b6a4b383add9ea4cafc
maetin <maetin0324@gmail.com>
2023-06-09 12:36:24 +0900

    create anker for about in header

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -2,11 +2,16 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className="sticky top-0 border-b z-10 ">
+    <header className="justify-self-center text-center font-zenkaku ">
       <div className="px-10 py-3 flex flex-row w-full items-center text-main fixed backdrop-blur-md h-16 select-none">
         <Link href="/" className="font-mono white">
           maetin.dev
         </Link>
+        <div className="ml-auto space-x-6 text-lg">
+        <Link href="/about" className="font-mono ">
+          about
+        </Link>
+      </div>
       </div>
     </header>
   );
