43f11b264028e58262d51f0ea58bfd5a1a159e1b
maetin0324 <maetin0324@users.noreply.github.com>
2023-06-09 14:52:02 +0000

    deploy: b5049ffe8c5d243a789be5036510dc95ac9c44ec

---
+++ _next/static/chunks/app/about/page-1c439edb060c978f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[301],{6931:function(n,e,u){Promise.resolve().then(u.t.bind(u,168,23))},168:function(){}},function(n){n.O(0,[667,139,744],function(){return n(n.s=6931)}),_N_E=n.O()}]);
