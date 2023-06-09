58f750706cc51ce004be24aaee3e69ac04b9d7c5
maetin <maetin0324@gmail.com>
2023-06-09 12:36:42 +0900

    add created by next.js in footer

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,8 +1,9 @@
 const Footer = () => {
   return (
     <footer className="bg-gray-100" style={{ background: '#d3def1' }}>
-      <div className="max-w-4xl w-full mx-auto h-24 flex items-center justify-center">
+      <div className="max-w-4xl w-full mx-auto h-24 flex items-center justify-center flex flex-col">
         <div className="font-mono">© 2023, maetin</div>
+        <div className="font-mono text-gray-500 text-sm">Created by Next.js</div>
       </div>
     </footer>
   );
