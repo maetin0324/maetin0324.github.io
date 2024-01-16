8199415494d634497675d7f2384ae3498a635530
maetin <maetin0324@gmail.com>
2024-01-16 23:07:43 +0900

    2024

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-100" style={{ background: '#d3def1' }}>
       <div className="max-w-4xl w-full mx-auto h-24 flex items-center justify-center flex flex-col">
-        <div className="font-mono text-slate-800">© 2023, maetin</div>
+        <div className="font-mono text-slate-800">© 2024, maetin</div>
         <div className="font-mono text-gray-500 text-sm">Created by Next.js</div>
       </div>
     </footer>
