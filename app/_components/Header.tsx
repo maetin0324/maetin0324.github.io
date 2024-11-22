25d959f5aecd7822277e6cdc928a50d541dc9214
maetin <maetin0324@gmail.com>
2024-11-22 16:25:57 +0900

    headerが壊れてたのを修正

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className="justify-self-center text-center font-zenkaku ">
+    <header className="justify-start text-center font-zenkaku ">
       <div className="px-10 py-3 flex flex-row w-full items-center text-main fixed backdrop-blur-md h-16 select-none">
         <Link href="/" className="font-mono text-slate-800">
           maetin.dev
